99cd0f303e688460735c0cb229e90478e5d5833a
rafi <82299860+rafi1919@users.noreply.github.com>
2022-09-28 13:14:13 +0700

    Add files via upload
    oi

---
+++ main.js
@@ -0,0 +1,11 @@
+function formAnak(val) 
+{ 
+    if(val==1)
+    {
+        document.getElementById('anak').style.display='block';
+    }
+    if(val==2)
+    {
+        document.getElementById('anak').style.display='none';
+    }
+ }
